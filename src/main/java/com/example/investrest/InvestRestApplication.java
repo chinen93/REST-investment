f100e1cac3204a3afb9a8d2692d55afc06573e4f
Pedro Chinen <chinen93@gmail.com>
2021-07-26 00:01:36 -0300

    Feat: initial structure with Spring Initializr

---
+++ src/main/java/com/example/investrest/InvestRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.investrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/investrest/InvestRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.investrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
