1521f7343d263efc1754a4d6a5fceeb2b00871f3
Pedro Chinen <chinen93@gmail.com>
2021-07-27 23:38:33 -0300

    Feat: create QuoteController to handle api calls

---
+++ src/main/java/com/example/investrest/controllers/QuoteController.java
@@ -0,0 +1,45 @@
+/**
+    https://spring.io/guides/gs/accessing-data-mysql/
+ */
+package com.example.investrest.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.example.investrest.models.Quote;
+import com.example.investrest.dto.QuoteDTO;
+import com.example.investrest.services.QuoteService;
+
+import java.time.LocalDate;
+
+
+@Controller // This means that this class is a Controller
+@RequestMapping(path="/quote") // This means URL's start with /quote (after Application path)
+public class QuoteController {
+
+	@Autowired
+    private QuoteService quoteService;
+
+    @PostMapping(path="/add") // Map ONLY POST Requests
+    public ResponseEntity<Quote> addNewQuote (@RequestBody QuoteDTO quoteDTO) {
+
+        Quote quote = quoteDTO.createQuote();
+        quoteService.saveQuote(quote);
+
+        return new ResponseEntity<>(quote, HttpStatus.CREATED);
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<Quote> getAllQuotes() {
+        // This returns a JSON or XML with the users
+        return quoteService.findAllQuotes();
+    }
+}
